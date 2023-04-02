531ac3263d1e07a88c3fd4a27cc5a6cdf7713267
Arihant Agnihotri <arihantagnihotri2106@gmail.com>
2023-04-03 02:01:57 +0530

    Add shadows to individual message box

--- client/src/components/chat/chat/Message.jsx
+++ client/src/components/chat/chat/Message.jsx
@@ -16,7 +16,7 @@ const Wrapper = styled(Box)`
     display: flex;
     border-radius: 10px;
     word-break: break-word;
-    box-shadow: 3px 5px  rgba(0,77,128,0.5);
+    box-shadow: 5px 5px  rgba(30,30,30,0.5);
 `;
     
 const Own = styled(Box)`
@@ -28,7 +28,7 @@ const Own = styled(Box)`
     display: flex;
     border-radius: 10px;
     word-break: break-word;
-    box-shadow: 3px 5px  rgba(0,77,128,0.5);
+    box-shadow: 5px 5px  rgba(0,77,128,0.5);
 `;
 
 const Text = styled(Typography)`
