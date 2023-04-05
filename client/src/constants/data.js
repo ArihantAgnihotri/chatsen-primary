f03b7890dbb8e6e37731cc4d26aa438f8b3c0203
Arihant Agnihotri <arihantagnihotri2106@gmail.com>
2023-04-06 03:01:05 +0530

    change chat app background

--- client/src/constants/data.js
+++ client/src/constants/data.js
@@ -4,5 +4,4 @@ export const iconPDF = 'https://cdn4.iconfinder.com/data/icons/logos-and-brands/
 export const defaultProfilePicture = 'https://static.straitstimes.com.sg/s3fs-public/articles/2020/12/01/af_moneyheist_011220.jpg';
 // export const emptyChatImage = 'https://i.gadgets360cdn.com/large/whatsapp_multi_device_support_update_image_1636207150180.jpg';
 export const emptyChatImage = 'https://gachi.gay/3Qm0y';
-export const qrCodeImage = 'https://www.ginifab.com/feeds/qr_code/img/qrcode.jpg';
 export const emptyProfilePicture = 'https://www.kindpng.com/picc/m/78-785827_user-profile-avatar-login-account-male-user-icon.png';
